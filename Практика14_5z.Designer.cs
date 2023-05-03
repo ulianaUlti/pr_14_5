e1b5cf306e490ffc3c97383d55e66de82fbad8b4
ulianaUlti <124128505+ulianaUlti@users.noreply.github.com>
2023-05-03 09:38:40 +0500

    Add files via upload

---
+++ Практика14_5z.Designer.cs
@@ -0,0 +1,163 @@
+﻿
+namespace prakt14_3_4_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(32, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Число n";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(175, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 69);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Заполнить очередь";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(261, 33);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(80, 69);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Извлечь очередь";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(36, 118);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(305, 251);
+            this.listBox1.TabIndex = 4;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(36, 68);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            300,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(434, 42);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(124, 69);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Вывод информации из файла <40";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(623, 42);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(116, 69);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Вывод информации отсортированную по возрасту";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(434, 118);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(305, 251);
+            this.listBox2.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ Практика14_5z.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+using System.Security.Policy;
+
+namespace prakt14_3_4_5
+{
+    public partial class Form1 : Form
+    {
+        Queue<int> queue = new Queue<int>();
+        Queue<string> people = new Queue<string>();
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void domainUpDown1_SelectedItemChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear(); 
+            queue.Clear();
+            int save = (int)numericUpDown1.Value;
+            for (int i = 1; i <= save; i++)
+            {
+                queue.Enqueue(i);
+            }
+            MessageBox.Show("Очередь успешно заполнена!", "Сообщение", MessageBoxButtons.OK);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (queue.Count == 0)
+            {
+                MessageBox.Show("Очередь не найдена!", "Сообщение", MessageBoxButtons.OK);
+            }
+            else
+            {
+                foreach (int num in queue)
+                {
+                    listBox1.Items.Add(num);
+                }
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            StreamReader st = new StreamReader("people.txt");
+            string str;
+            while (!st.EndOfStream)
+            {
+                str = st.ReadLine();
+                string[] spl = str.Split(' ');
+                int age = Convert.ToInt32(spl[3]);
+                if (age < 40)
+                {
+                    listBox2.Items.Add(str);
+                }
+                else people.Enqueue(str);
+            }
+            st.Close();
+            foreach (string lines in people)
+            {
+                listBox2.Items.Add(lines);
+            }
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
+
